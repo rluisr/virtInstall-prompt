f3b0d1d0cc2c74b80a3c3354190e56f85b13765f
root <root@kvm.luis.local>
2016-10-09 23:17:48 +0900

    A

---
+++ create_vm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+
+readonly CURRENT_PATH=$(cd $(dirname $0) && pwd)
+. ${CURRENT_PATH}/lib/function.sh
+
+if [ $# -eq 0 ]; then
+  prepare_setup
+  show_specs
+
+  green "Are you sure you want to install? [y/N]: "
+  while read value
+  do
+    case ${value} in
+      "y" | "Y" ) setup ;;
+      "n" | "N" ) red "Cancelled."; exit 1 ;;
+      * ) continue ;;
+    esac
+  done
+
+elif [ $# -gt 0 ]; then
+  case $1 in
+    "-v" | "--version" ) version ;;
+    "-h" | "--help" ) usage ;;
+    * ) usage ;;
+  esac
+fi
