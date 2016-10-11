87d7bde930c2ea3a8320b6615ab7fc0eafefe9f3
rluisr <detteiu.x14th@gmail.com>
2016-10-11 23:20:49 +0900

    Add function for discriminating OS

--- lib/function.sh
+++ lib/function.sh
@@ -29,6 +29,19 @@ blue() {
   echo -e "$BLUE$*$NORMAL"
 }
 
+checkOS() {
+  if [ "$(uname)" == 'Darwin' ]; then
+  OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  OS='Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
+  OS='Cygwin'
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
+}
+
 usage() {
   cat <<EOF
   $(basename ${0}) is tool for creating new virtual machine with prompt.
